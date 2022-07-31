2d60aa41c2a6012ed63622d4479cfb0ed18e456a
ki <decyberprince@gmail.com>
2022-07-31 12:52:11 +0500

    - resizer finished

--- src/components/table/TableComponent.js
+++ src/components/table/TableComponent.js
@@ -1,14 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from "@src/components/table/table.template";
-import {$} from '@core/dom';
+import {createTable} from '@src/components/table/table.template'
+import {TableResizer} from '@src/components/table/table.resize'
 
 export class TableComponent extends ExcelComponent {
-
-  #resizer = {
-    selector: '',
-    styles: {}
-  }
-
   static className = 'excel__table'
   constructor($root) {
     super($root, {
@@ -20,50 +14,12 @@ export class TableComponent extends ExcelComponent {
     return createTable()
   }
   onMousedown(e) {
-    const $resizer = $(e.target)
-    if (!$resizer.data.resize) return
-    const $parent = $resizer.closest('[data-type=resizable]')
-    const start = $parent.coords
-    const type = $resizer.data.resize
-
-    this.#resizer.selector = `[data-${type}="${$parent.data[type]}"]`
-    $resizer.data.active = ''
-
-    document.onmousemove = docE => {
-      if (type === 'column') {
-        const dx = docE.pageX - start.right
-        if (start.width + dx > 0) {
-          $resizer.css({right:`${-dx}px`})
-          this.#resizer.styles = {width:`${start.width + dx}px`}
-        }
-      } else {
-        const dy = docE.pageY - start.bottom
-        if (start.height + dy > 0) {
-          $resizer.css({bottom:`${-dy}px`})
-          this.#resizer.styles = {height: `${start.height + dy}px`}
-        }
-
-      }
-
-
-    }
-
-    document.onmouseup = () => {
-      delete $resizer.data.active
-      $resizer.css({right:0, bottom: 0})
-      document.onmousemove = null
-      this.$root.queryAll(this.#resizer.selector)
-        .forEach(el => {
-        $(el).css(this.#resizer.styles)
-      })
-    }
-
+    const $resizer = new TableResizer(e.target, this.$root)
+    if (!$resizer.isResizer) return
+    document.onmousemove = $resizer.start()
+    document.onmouseup = () => $resizer.end()
   }
-  onMousemove(e) {
+  onMousemove(e) {}
 
-  }
-
-  onMouseup() {
-
-  }
+  onMouseup() {}
 }
